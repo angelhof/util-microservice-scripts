f9b2a026412539afeb05dc7c6a6b5723f3c8bb0d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-10-31 10:36:39 -0400

    install helm

---
+++ helm-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing helm 3..."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+echo "helm 3 successfully installed"
+
